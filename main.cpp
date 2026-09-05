3fb5a5a655230b1f3f5657addaaa70a89a1f59f6
Franklin <franklinguitar2004@icloud.com>
2026-09-05 14:23:23 -0400

    Added loop test in main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "6502.h"
+#include <iostream>
 
 int main()
 {
@@ -6,24 +7,60 @@ int main()
     cpu.Initialize();
 
     //inline instruction - loop
-    // load val in to X for num of iterations
-    // call CPX and then BEQ
-    // otherwise DEX
+    /*loop
+
+      LDX
+
+      CPX
+      BEQ => jump to address if zero flag is on
+      DEX
+      JMP
+    */
+
+    uint8_t num_iterations = 0x05;
 
 
-    std::cout << "Accumlator Register: " << (int)cpu.A << std::endl;
-    std::cout << "X register: " << (int)cpu.X << std::endl;
-    std::cout << "Y register: " << (int)cpu.Y << std::endl;
-    std::cout << "Program Counter: " << std::format("{:#X}\n", (int)cpu.PC) << std::endl;
 
-    std::cout << "Status: ";
+    cpu.mem[0x8000] = 0xA2; // LDX
+    cpu.mem[0x8001] = num_iterations;
+    cpu.Clock();
+    cpu.Clock();
+
+    cpu.mem[0x8002] = 0xE0; // CPX
+    cpu.mem[0x8003] = 0x00;
+
+    cpu.mem[0x8004] = 0xF0; // BEQ
+    cpu.mem[0x8005] = 0x04;
+
+    cpu.mem[0x8006] = 0xCA; // DEX
+
+    cpu.mem[0x8007] = 0x4C; // JMP
+    cpu.mem[0x8008] = 0x02;
+    cpu.mem[0x8009] = 0x80;
+
+    while (true) {
+        char in;
+        std::cout << "Type c to clock CPU: ";
+        std::cin >> in;
+
+        if (in == 'c') {
+            std::cout << std::endl;
+            std::cout << "Cycles: " << (int)cpu.cycles << std::endl;
+            std::cout << "X register: " << (int)cpu.X << std::endl;
+            std::cout << "Program Counter: " << std::format("{:#X}\n", (int)cpu.PC);
+            std::cout << "Status: ";
 
 
-    for (int i = 7; i > -1; i--) {
-        int on = (cpu.status & (1 << (i))) > 0;
-        std::cout << on;
+            for (int i = 7; i > -1; i--) {
+                int on = (cpu.status & (1 << (i))) > 0;
+                std::cout << on;
+            }
+            std::cout << std::endl;
+            std::cout << std::endl;
+            cpu.Clock();
+
+        }
     }
-    std::cout << std::endl;
 
     return 0;
 }
