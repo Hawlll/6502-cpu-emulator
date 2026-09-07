5585a4b25ae02aea9a79cd4db2a09e7815c41c03
Franklin <franklinguitar2004@icloud.com>
2026-09-07 18:48:30 -0400

    Visualized Vram on gpu

--- main.cpp
+++ main.cpp
@@ -25,20 +25,58 @@ int main()
     */
 
     bus.Write(0x8000, 0xA9);
-    bus.Write(0x8001, 0x42);
-    bus.Write(0x8002, 0x8D);
-    bus.Write(0x8003, 0x01);
-    bus.Write(0x8004, 0xE0);
-
-    cpu.Clock(bus);
-    cpu.Clock(bus);
-    cpu.Clock(bus);
-    cpu.Clock(bus);
+    bus.Write(0x8001, 0x01);
     cpu.Clock(bus);
     cpu.Clock(bus);
 
-    std::cout << (int)ram.Read(0xE001) << " " << (int)gpu.Read(0x0001) << std::endl;
 
+    uint16_t test_vram_locs[] = {
+        0xE033, 0xE034, 0xE035, 0xE036,
+
+        0xE043,
+        0xE053,
+        0xE063,
+        0xE073,
+        0xE083,
+        0xE093,
+        0xE0A3,
+        0xE0B3,
+
+        0xE047,
+        0xE057,
+        0xE067,
+
+        0xE073, 0xE074, 0xE075, 0xE076,
+
+        0xE087,
+        0xE097,
+        0xE0A7,
+
+        0xE0B3, 0xE0B4, 0xE0B5, 0xE0B6
+    };
+
+    for (size_t i = 0; i < std::size(test_vram_locs); i++) {
+        bus.Write(0x8002 + (3 * i), 0x8D);
+        bus.Write(0x8003 + (3 * i), test_vram_locs[i] & 0x00FF);
+        bus.Write(0x8004 + (3 * i), (test_vram_locs[i] & 0xFF00) >> 8);
+    }
+
+    for (int i = 0; i < (int)std::size(test_vram_locs) * 4; i++) {
+        cpu.Clock(bus);
+    }
+
+    for (int x = 0; x < 16; x++) {
+        for (int y = 0; y < 16; y++) {
+            uint8_t val_in_vram = bus.Read(0xE000 + (x*16) + y);
+            if (val_in_vram == 0x01) {
+                std::cout << "#";
+            }
+            else {
+                std::cout << ".";
+            }
+        }
+        std::cout << std::endl;
+    }
 
 //    uint8_t num_iterations = 0x01;
 //
